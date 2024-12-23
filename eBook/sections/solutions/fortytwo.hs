67c2ae5ee1a1538af355742718a17900786b7b14
Raul <42917076+elRaulito@users.noreply.github.com>
2024-12-23 14:30:08 +0100

    Pushed new chapters

---
+++ eBook/sections/solutions/fortytwo.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE TemplateHaskell     #-}
+
+module FortyTwo where
+
+import qualified Plutus.V2.Ledger.Api as PlutusV2
+import           PlutusTx             (BuiltinData, compile)
+import           PlutusTx.Builtins    as Builtins (mkI)
+import           PlutusTx.Prelude     (otherwise, traceError, (==))
+import           Prelude              (IO)
+import           Utilities            (writeValidatorToFile)
+
+-- This validator succeeds only if the redeemer is 42
+--                  Datum         Redeemer     ScriptContext
+mk42Validator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mk42Validator _ r _
+    | r == Builtins.mkI 42 = ()
+    | otherwise            = traceError "expected 42"
+{-# INLINABLE mk42Validator #-}
+
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mk42Validator ||])
+
+saveVal :: IO ()
+saveVal = writeValidatorToFile "./assets/fortytwo.plutus" validator
