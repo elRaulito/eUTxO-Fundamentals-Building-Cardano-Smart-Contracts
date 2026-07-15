7bdacc97cbb7f57c35cec541bf5762e61946e998
Raul <42917076+elRaulito@users.noreply.github.com>
2026-07-15 10:50:18 +0200

    Lucid obsolete conversion to Mesh

--- eBook/sections/solutions/delist.js
+++ eBook/sections/solutions/delist.js
@@ -1,20 +1,23 @@
-import { Lucid , Blockfrost,toUnit,Data,Constr,  fromHex,
-    toHex,sha256  } from "https://unpkg.com/lucid-cardano@0.10.0/web/mod.js"
-  const lucid = await Lucid.new(
-    new Blockfrost("https://cardano-mainnet.blockfrost.io/api/v0", "API KEY"),
-    "Mainnet",
-  );
-  
+import {
+    BlockfrostProvider,
+    BrowserWallet,
+    MeshTxBuilder,
+    deserializeAddress,
+    mConStr0,
+  } from "@meshsdk/core"
+
+  const provider = new BlockfrostProvider("API KEY");
+
+  //contract.json contains the double CBOR encoded script, for example { "type": "PlutusV2", "script": "59..." }
   import cnftScript from './contract.json' assert {type: 'json'};
 
    //Here replace with the wallet name according to cip30
-   var wallet="eternl"
+   var walletName="eternl"
 
-   let api = await window.cardano[wallet].enable();
-   lucid.selectWallet(api);
-   window.owner=await lucid.wallet.address()
-   let{ paymentCredential,stakeCredential } = lucid.utils.getAddressDetails(
-   await lucid.wallet.address(),
+   const wallet = await BrowserWallet.enable(walletName);
+   window.owner=await wallet.getChangeAddress()
+   const { pubKeyHash: paymentCredentialHash } = deserializeAddress(
+   window.owner,
    );
 
   //this is the most tricky part, go to https://cbor.me/ and paste this datum, replace the fields with your listing data and get the new datum to replace this one
@@ -26,24 +29,44 @@ import { Lucid , Blockfrost,toUnit,Data,Constr,  fromHex,
 
 
 
-    var redeemerCancel=Data.to(
-      new Constr(0,[])
-    )
+    var redeemerCancel=mConStr0([])
 
 
     //THIS IS THE TXHASH OF THE LISTING TO BE CANCELLED REPLACE IT
     const utxoHash="dbd761d11fa7dde62c4899b0168c6618789ad68d34560e9fd7a40e5d498d3044"
     const index=0
 
-    var utxo=await lucid.utxosByOutRef([{ txHash: utxoHash, outputIndex: index }])
-    utxo[0].datum=datumCancel
+    const [scriptUtxo]=await provider.fetchUTxOs(utxoHash, index)
 
-    const tx = await lucid
-    .newTx()
-    .collectFrom(utxo,redeemerCancel)
-    .attachSpendingValidator(cnftScript)
-    .addSignerKey(paymentCredential.hash)
+    const utxos=await wallet.getUtxos()
+    const collateral=(await wallet.getCollateral())[0]
+
+    const txBuilder = new MeshTxBuilder({
+      fetcher: provider,
+      submitter: provider,
+    });
+
+    await txBuilder
+    .spendingPlutusScriptV2()
+    .txIn(
+      scriptUtxo.input.txHash,
+      scriptUtxo.input.outputIndex,
+      scriptUtxo.output.amount,
+      scriptUtxo.output.address
+    )
+    .txInScript(cnftScript.script)
+    .txInDatumValue(datumCancel, "CBOR")
+    .txInRedeemerValue(redeemerCancel)
+    .requiredSignerHash(paymentCredentialHash)
+    .txInCollateral(
+      collateral.input.txHash,
+      collateral.input.outputIndex,
+      collateral.output.amount,
+      collateral.output.address
+    )
+    .changeAddress(window.owner)
+    .selectUtxosFrom(utxos)
     .complete();
 
-    const signedTx = await tx.sign().complete();
-    const txHash = await signedTx.submit();
+    const signedTx = await wallet.signTx(txBuilder.txHex);
+    const txHash = await wallet.submitTx(signedTx);
